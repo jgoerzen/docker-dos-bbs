35dc72f14bdc07dd3853c4b99b56098e0623c842
John Goerzen <jgoerzen@complete.org>
2017-05-08 00:14:43 -0500

    tweak

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,6 @@ FROM jgoerzen/qemu-dos
 
 MAINTAINER John Goerzen <jgoerzen@complete.org>
 
-RUN mkdir /tmp/setup
-COPY setup/sums /tmp/setup
-COPY setup/doorsums /tmp/setup
-# Do the download ASAP so we don't hit the download sites overly hard
-COPY setup/download.sh /tmp/setup/download.sh
-RUN /tmp/setup/download.sh
-
 COPY setup/ /tmp/setup/
 COPY supervisor/ /etc/supervisor/conf.d/
 COPY scripts/ /usr/local/bin/
--- setup/setup.sh
+++ setup/setup.sh
@@ -6,17 +6,34 @@ set -x
 echo 0 > /dos/numnodes
 echo 1 > /dos/startconsole
 
+## Do the downloads
+cd /tmp/setup
+wget https://ftp.sunet.se/mirror/archive/ftp.sunet.se/pub/simtelnet/msdos/fossil/adf_150.zip
+wget http://www.starbase21.net/tm421.zip
+wget https://archive.org/download/TelixCommunications_1020/telix351.zip
+
+sha256sum -c < sums
+
+mkdir doors
+cd doors
+wget http://archives.scovetta.com/pub/fehq/BBSDoorGames/2002v309.zip
+wget http://www.slbbs.com/files/doorgame/lord407.zip
+wget http://www.slbbs.com/files/doorgame/lord407-patch.zip
+sha256sum -c < ../doorsums
+
 # Unpack ADF (FOSSIL)
 cd /dos/drive_h
 mkdir adf
 cd adf
 unzip /tmp/setup/adf_150.zip
+rm /tmp/setup/adf_150.zip
 
 # Set up Telemate
 cd /dos/drive_g
 mkdir comm
 cd comm
 unzip /tmp/setup/tm421.zip
+rm /tmp/setup/tm421.zip
 mv tm421 TM
 cd TM
 sed -i 's/Baud=38400/Baud=57600/' TM.CFG
@@ -30,23 +47,28 @@ mkdir TELIX
 mkdir telix_extract
 cd telix_extract
 unzip /tmp/setup/telix351.zip
+rm /tmp/setup/telix351.zip
 cd ../TELIX
 unzip ../telix_extract/TELIX351.DAT
 rm -r /dos/drive_g/comm/telix_extract
-mv -vi /tmp/setup/TELIX.* /dos/drive_g/comm/TELIX/
+mv -v /tmp/setup/TELIX.* /dos/drive_g/comm/TELIX/
 
 
-# Download doors
+# set up doors
 cd /dos/drive_g
 mkdir doors
 cd doors
 mkdir tw2002
 cd tw2002
 unzip /tmp/setup/doors/2002v309.zip
+rm /tmp/setup/doors/2002v309.zip
 cd ..
 mkdir lord
 cd lord
 unzip -L /tmp/setup/doors/lord407.zip
+rm /tmp/setup/doors/lord407.zip
 unzip -L lord.zip
+rm lord.zip
 unzip -o -L /tmp/setup/doors/lord407-patch.zip
+rm /tmp/setup/doors/lord407-patch.zip
 
