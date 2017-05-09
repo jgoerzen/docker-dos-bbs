e94283e4b425290640f1e860238d5145a070f409
John Goerzen <jgoerzen@complete.org>
2017-05-09 00:14:17 -0500

    Set timemode

--- setup/setup.sh
+++ setup/setup.sh
@@ -7,6 +7,7 @@ echo 0 > /dos/numnodes
 echo 1 > /dos/startconsole
 
 echo '$_com1 = "virtual"' >> /etc/dosemu/dosemu.conf
+echo '$_timemode = "linux"' >> /etc/dosemu/dosemu.conf
 mkdir /dos/drive_h/STARTUP
 
 ## Do the downloads
